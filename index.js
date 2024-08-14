6c164a89011d50a2515352935ec8c7efe4e0dfbf
Erick559 <erickungu82@gmail.com>
2024-08-14 16:20:08 +0300

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => res.type('html').send(`<!DOCTYPE html><p>Hello There</p></html>`));
 
 app.get('/ping', (req, res) => {
-    res.status(200).json({ status: 'ok' });
+    res.status(200).json({ message:'Server responded well',status: 'ok' });
 });
 
 app.listen(port, ()=> console.log(`listening on ${port}`));
