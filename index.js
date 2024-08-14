a7f9bd5d2a8ffad1fee837c6eeee1c86ef471bb7
Erick559 <erickungu82@gmail.com>
2024-08-14 15:44:25 +0300

    ping

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.type('html').send(`<!DOCTYPE html><p>Hello There</p></html>`));
 
+app.get('/ping', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+});
+
 app.listen(port, ()=> console.log(`listening on ${port}`));
