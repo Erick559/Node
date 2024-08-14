0ea9713f89be5dc3c3df6442be4c8ad2799a4e02
Erick559 <erickungu82@gmail.com>
2024-08-14 15:37:05 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.type('html').send(`<!DOCTYPE html><p>Hello There</p></html>`));
+
+app.listen(port, ()=> console.log(`listening on ${port}`));
